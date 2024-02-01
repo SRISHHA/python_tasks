86294bc378dbec1f4497680d6ad0ae8eabc5e38d
SRISHHA <89242466+SRISHHA@users.noreply.github.com>
2024-02-01 16:03:21 +0530

    Update simple3.js

--- jquery_task/simple3.js
+++ jquery_task/simple3.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    var rows = $('tbody tr');
+    var rows = $('tbody tr');//so it gives you back an array of row elements
 
     for (var i = 0; i < rows.length; i++) {
-        var row = rows[i];
+        var row = rows[i];//accessing the element in the array
         var name = $(row).find('td:eq(0)').text();
         var age = $(row).find('td:eq(1)').text();
         var prgmlang = $(row).find('td:eq(2)').text();
